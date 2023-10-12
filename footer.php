4f5fe764516962e6dabb8937c50dcf7eb3e0c24f
Mukto <muktoapb@gmail.com>
2023-10-12 12:24:27 +0600

    header footer and home template setup done

--- footer.php
+++ footer.php
@@ -1,14 +1,26 @@
 
 <footer>
         <div class="container">
-            <div class="social">
-                <a href="#">facebook </a>
-                <a href="#">twitter </a>
-                <a href="#">instagram </a>
-                <a href="#">google plus </a>
-                <a href="#">behance </a>
-                <a href="#">dribbble  </a>
-            </div>
+            <?php if ( have_rows('social_links','option') ) : ?>
+                <div class="social">
+                <?php while( have_rows('social_links','option') ) : the_row(); ?>
+            
+                    <?php 
+                    $link = get_sub_field('link');
+                    
+                    if( $link ):
+                        $link_url = $link['url'];
+                        $link_title = $link['title'];
+                        $link_target = $link['target'] ? $link['target'] : '_self';
+                        ?>
+                        <a href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>"><?php echo esc_html($link_title); ?></a>
+                    <?php endif; ?>
+                <?php endwhile; ?>
+                </div>
+            <?php endif; ?>
+            
+        
+            
             <?php if ( get_field('copyright_text', 'option') ) : ?>
                 <?php echo get_field('copyright_text', 'option'); ?>
             <?php endif; ?>
